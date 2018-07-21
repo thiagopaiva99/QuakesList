1336bb1d3dc97f9d4d5419812b130b7dcbf201cc
thiagopaiva99 <thiagopaiva99@gmail.com>
2018-07-21 14:54:11 -0300

    Creating the application AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -4,5 +4,23 @@ import 'dart:convert';
 import 'package:http/http.dart';
 
 void main() async {
+  runApp(new MaterialApp(
+    title: 'Quakes',
+    home: new Quakes(),
+  ));
+}
 
+class Quakes extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(
+          'Quakes'
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
 }
