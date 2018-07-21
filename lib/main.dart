86a55fa3d3dfaf4c355bc12b780bed3a8e768444
thiagopaiva99 <thiagopaiva99@gmail.com>
2018-07-21 15:06:24 -0300

    Creating the func to search for quakes on API

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:convert';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 void main() async {
   runApp(new MaterialApp(
@@ -23,4 +23,11 @@ class Quakes extends StatelessWidget {
       ),
     );
   }
+}
+
+Future<List> getQuakes() async {
+  String apiUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
+  http.Response response = await http.get(apiUrl);
+
+  return JSON.decode(response.body);
 }
